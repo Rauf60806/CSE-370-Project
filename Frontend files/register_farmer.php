ce245fe35a037ce08aec037524108c524c2702d5
Rauf60806 <rauf60806@gmail.com>
2025-12-14 10:07:18 +0600

    DBfiles

--- Frontend files/register_farmer.php
+++ Frontend files/register_farmer.php
@@ -3,18 +3,20 @@ require "db.php";
 $data = json_decode(file_get_contents("php://input"), true);
 
 $conn->query("
-INSERT INTO farm_owner (Farm_name,farm_location)
-VALUES ('{$data['farm_name']}','{$data['farm_location']}')
-");
-
-$conn->query("
-INSERT INTO dashboard_panel (user_name,password)
-VALUES ('{$data['username']}','{$data['password']}')
+INSERT INTO dashboard_panel (user_name,Farm_name,farm_location,password)
+VALUES ('{$data['username']}','{$data['farm_name']}','{$data['farm_location']}','{$data['password']}')
 ");
 
 $conn->query("
 INSERT INTO registers (registration_id,access_key,date)
 VALUES (LAST_INSERT_ID(),{$data['access_key']},CURDATE())
 ");
+$conn->query("
+INSERT INTO admin_panel (access_key,registration_id,date)
+VALUES ({$data['access_key']},LAST_INSERT_ID(),CURDATE())
+");
 
 echo json_encode(["success"=>true]);
+header("location: index.php");
+?>
+
