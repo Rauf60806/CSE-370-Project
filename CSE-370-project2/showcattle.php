468064407bcae2f9128564cee6702f51acab32c9
Rauf60806 <rauf60806@gmail.com>
2025-12-24 20:49:16 +0600

    fixed

--- CSE-370-project2/showcattle.php
+++ CSE-370-project2/showcattle.php
@@ -27,8 +27,6 @@
                 session_start();
                 $sql="SELECT * from cattle c join owns_cattle o on c.cattle_id = o.cattle_id group by user_name having user_name = '"
     .               mysqli_real_escape_string($conn, $_SESSION['user']) . "'";
-                require_once "db.php";
-                $sql="select * from cattle c";
                 $result=mysqli_query($conn,$sql);
                 if (mysqli_num_rows($result)> 0) {
                 while ($row=mysqli_fetch_assoc($result)) {
